a86dc69e910f6b6521dc455a6d340cf729f597da
aleclivinghouse <aleclivinghouse@yahoo.com>
2020-11-01 11:30:25 -0800

    locators finished

---
+++ src/main/java/pages/locators/HomeLocators.java
@@ -0,0 +1,73 @@
+package pages.locators;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class HomeLocators {
+	
+	//post locators
+	//post input
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[1]/form/div[1]/textarea")
+	public WebElement postInput;
+	
+	//post submit
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[1]/form/div[2]/button")
+	public WebElement postSubmit;
+	
+	//post photo button
+	@FindBy(how=How.ID ,using="post-pic-input")
+	public WebElement postPic;
+	
+	//first post text
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[2]/div/div[1]")
+	public WebElement FirstPostText;
+	
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[1]/div[2]/a")
+	public WebElement firstPostAuthor;
+	
+	//tagged user in post
+	//NOTE this will have to be changed
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[2]/div[1]/div[1]/div[2]/div[1]/a")
+	public WebElement firstPostTaggedUser;
+	
+	// first post like button
+	@FindBy(how=How.ID ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[3]/div[1]")
+	public WebElement likeFirstPost;
+	
+	//first post like count
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[2]/div[1]")
+	public WebElement firstPostLikeCount;
+	
+	//first post edit/delete dropdown
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[1]/div[3]]")
+	public WebElement firstPostEditDeleteDropdown;
+	
+	//first post edit
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[1]/div[3]/div/ul/li[1]")
+	public WebElement firstPostEdit;
+	
+	//first post delete
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[2]/div/div[2]/div[1]/div[1]/div[3]/div/ul/li[2]")
+	public WebElement firstPostDelete;
+	
+	//comment locators
+	//comment on first post input
+	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[2]/div/div[2]/div[2]/div[2]/div/div)[last()]/form/input")
+	public WebElement firstPostCommentInput;
+	
+	//the last comment on the first post
+	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[2]/div/div[2]/div[2]/div[2]/div/div)[last()-1]/div[2]/div[2]")
+	public WebElement firstPostLastComment;
+	
+	//author of the last comment made on the first post
+	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[2]/div/div[2]/div[2]/div[2]/div/div)[last()-1]/div[2]/div[1]")
+	public WebElement firstPostLastCommentAuthor;
+	
+	//delete the last comment on the first post
+	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[2]/div/div[2]/div[2]/div[2]/div/div)[last()-1]/div[3]")
+	public WebElement firstPostLastCommentDelete;
+	
+	
+
+}
--- src/main/java/pages/locators/OtherProfileLocators.java
+++ src/main/java/pages/locators/OtherProfileLocators.java
@@ -1,58 +1,40 @@
 package pages.locators;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class ProfileLocators {
-
-	//cover photo
-	@FindBy(how=How.ID ,using="profile-cover-input")
-	public WebElement uploadCoverPhoto;
+public class OtherProfileLocators {
+	//add friend button
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[1]/div[1]/div[2]/div/div")
+	public WebElement addFriend;
 	
-	//change profile info
-	@FindBy(how=How.NAME ,using="birthday")
-	public WebElement changeBirthDay;
+	//Name
+	@FindBy(how=How.ID ,using="profile-cover-name")
+	public WebElement profileName;
 	
-	@FindBy(how=How.NAME ,using="relationship")
-	public WebElement changeRelationShipStatus;
+	//post locators
+	//post input
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[1]/form/div[1]/textarea")
+	public WebElement postInput;
 	
-	@FindBy(how=How.NAME ,using="school")
-	public WebElement changeSchool;
+	//post submit
+	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[1]/form/div[2]/button")
+	public WebElement postSubmit;
 	
-	@FindBy(how=How.NAME ,using="workplace")
-	public WebElement changeWork;
-	
-	@FindBy(how=How.NAME ,using="current_town")
-	public WebElement changeCurrentTown;
-	
-	@FindBy(how=How.XPATH ,using="//*[@id=\"root\"]/div/section/div[2]/div/div")
-	public WebElement aboutUserSubmit;
-	
-	//profile pic 
-	@FindBy(how=How.ID ,using="profile-pic-input")
-	public WebElement changeProfilePic;
-	
-	//post form
-	@FindBy(how=How.NAME ,using="body")
-	public WebElement postTextArea;
-	
+	//post photo button 
 	@FindBy(how=How.ID ,using="post-pic-input")
 	public WebElement postPic;
 	
+	@FindBy(how=How.XPATH ,using="//*[@id=\"root\"]/div/section/div[3]/div/div[2]/div[1]/div[2]/div/div[1]")
+	public WebElement firstPostInFeed;
+	
+	//first post author
 	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[2]/div[1]/div[1]/div[2]/a")
 	public WebElement firstPostAuthor;
 	
 	//tagged user in post
 	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[2]/div[1]/div[1]/div[2]/div[1]/a")
 	public WebElement firstPostTaggedUser;
-	
-	@FindBy(how=How.XPATH ,using="//*[@id=\"root\"]/div/section/div[3]/div/div[1]/form/div[2]/button")
-	public WebElement postSubmit;
-	
-	//the text of the first post on the profile feed
-	@FindBy(how=How.XPATH ,using="//*[@id=\"root\"]/div/section/div[3]/div/div[2]/div[1]/div[2]/div/div[1]")
-	public WebElement firstPostInFeed;
 	
 	//like the first post in the feed
 	@FindBy(how=How.XPATH ,using="/html/body/main/div/section/div[3]/div/div[2]/div[1]/div[3]/div[1]")
@@ -79,6 +61,10 @@ public class ProfileLocators {
 	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[3]/div/div[2]/div[2]/div/div/div)[last()-1]/div[2]/div[2]")
 	public WebElement firstPostLastComment;
 	
+	//author of the last comment made on the first post
+	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[3]/div/div[2]/div[2]/div/div/div)[last()-1]/div[2]/div[1]")
+	public WebElement firstPostLastCommentAuthor;
+	
 	//delete the last comment made on the first post in the feed
 	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[3]/div/div[2]/div[2]/div/div/div)[last()-1]/div[3]")
 	public WebElement firstPostLastCommentDelete;
@@ -86,8 +72,5 @@ public class ProfileLocators {
 	//The input field to write a comment on the first post in the feed
 	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[3]/div/div[2]/div[2]/div/div/div)[last()]/form/input")
 	public WebElement firstPostCommentInput; 
-	
-	@FindBy(how=How.XPATH ,using="(/html/body/main/div/section/div[3]/div/div[2]/div[2]/div/div/div)[last()-1]/div[2]/div[1]")
-	public WebElement firstPostLastCommentAuthor;
-	
+
 }
